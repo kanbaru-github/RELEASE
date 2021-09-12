33024d546e9866a579b3d11f9c2aaf26150c4dd1
kanbaru-github <usupfv@gmail.com>
2021-09-12 11:55:58 +0000

    ログインチェック追加

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,4 +1,7 @@
 class Admin::SessionsController < Devise::SessionsController
+
+  before_action :not_customer
+
   def after_sign_in_path_for(resource)
     admin_posts_path
   end
@@ -6,4 +9,11 @@ class Admin::SessionsController < Devise::SessionsController
   def after_sign_out_path_for(resource)
     new_admin_session_path
   end
+
+  def not_customer
+    if customer_signed_in?
+      redirect_to root_path
+    end
+  end
+
 end
--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,5 +1,7 @@
 class Public::SessionsController < Devise::SessionsController
+
   before_action :reject_inactive_customer, only: [:create]
+  before_action :not_admin
 
   def after_sign_in_path_for(resource)
     posts_path
@@ -18,4 +20,11 @@ class Public::SessionsController < Devise::SessionsController
       end
     end
   end
+
+  def not_admin
+    if admin_signed_in?
+      redirect_to root_path
+    end
+  end
+
 end
