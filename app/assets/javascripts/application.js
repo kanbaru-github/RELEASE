ba7e6ee3c0da649a770fb79e9281388de5127dc6
kanbaru-github <usupfv@gmail.com>
2021-09-24 12:46:41 +0000

    タブメニュー追加

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -78,3 +78,20 @@ $('.search__form__input').on('keyup', function(e){
   Rails.fire($(".search__form")[0], "submit");
   });
 });
+
+// タブメニュー
+$(function() {
+    // ↓タブをクリックした時の処理↓
+    $('.tab').click(function(){
+        // ↓現在アクティブなタブを切り替え↓
+        $('.tab-active').removeClass('tab-active');
+        // ↓タブをアクティブにする↓
+        $(this).addClass('tab-active');
+        // ↓現在アクティブなタブの中身を非表示
+        $('.box-show').removeClass('box-show');
+        // ↓クリックしたタブから順番を取得↓
+        const index = $(this).index();
+        // ↓クリックしたタブと同じ順番のコンテンツを表示↓
+        $('.tabbox').eq(index).addClass('box-show');
+    });
+});
