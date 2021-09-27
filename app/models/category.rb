e689bf564b66665736c8c54d05257ca04ba2953f
kanbaru-github <usupfv@gmail.com>
2021-09-27 14:35:35 +0000

    感情分析スコアをタグに変換

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,5 @@ class Category < ApplicationRecord
   has_many :posts
 
   validates :name, presence: true, uniqueness: true
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_02_054154) do
+ActiveRecord::Schema.define(version: 2021_09_27_124430) do
 
   create_table "admins", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -81,6 +81,7 @@ ActiveRecord::Schema.define(version: 2021_09_02_054154) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.decimal "score", precision: 5, scale: 3
+    t.string "emotions"
   end
 
   create_table "relationships", force: :cascade do |t|
