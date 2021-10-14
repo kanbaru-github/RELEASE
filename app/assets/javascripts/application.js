bf5687b0e9055e75d10bb6d9390f4aa3e3278508
kanbaru-github <usupfv@gmail.com>
2021-10-14 14:59:57 +0000

    コメント追加

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -100,7 +100,6 @@ $('.search__form__input').on('keyup', function(e){
 window.addEventListener('DOMContentLoaded', ()=> {
   // コンテナを指定
   const container = document.querySelector('.leaves-container');
-
   // 葉っぱを生成する関数
   const createLeaf = (leafClass, minSizeVal, maxSizeVal) => {
     const leafEl = document.createElement('span');
@@ -112,13 +111,11 @@ window.addEventListener('DOMContentLoaded', ()=> {
     leafEl.style.height = `${size}px`;
     leafEl.style.left = Math.random() * 100 + '%';
     container.appendChild(leafEl);
-
     // 一定時間が経てば葉っぱを消す
     setTimeout(() => {
       leafEl.remove();
     }, 8000);
   }
-
   // 葉っぱを生成する間隔をミリ秒で指定する
   // createLeafの引数には、'クラス名', '最小サイズ', '最大サイズ'を渡す
   setInterval(createLeaf.bind(this, 'leaf-1', 30, 50), 1000);
